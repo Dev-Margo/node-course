5721542e1cc085307e5001808a5f870c56c4632a
MARGO STEPKA <MSTEPKA@HANOVER.COM>
2021-09-08 15:46:16 -0400

    added placeholder framework for add, remove, list, and read commands

--- node-notes/app.js
+++ node-notes/app.js
@@ -3,5 +3,45 @@ const yargs = require('yargs')
 const chalk = require('chalk')
 const getNotes = require('./notes')
 
-console.log(process.argv)
+//customize yargs version
+yargs.version('1.1.0')
+
+//create add comamand
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    handler: function () {
+        console.log('adding a new note!')
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    handler: function () {
+        console.log('removing the note')
+    }
+})
+
+//create list command
+yargs.command({
+    command: 'list',
+    describe: 'list all notes',
+    handler: function () {
+        console.log('listing notes')
+    }
+})
+
+//create read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    handler: function () {
+        console.log('reading a note')
+    }
+})
+
+//add, remove, read, and list notes.
+
 console.log(yargs.argv)
